ecdd4842423dc105194244ac627d2b395a13e18a
hetgwi01 <hetgwi03@gmail.com>
2026-03-24 13:10:50 +0900

    블로그 최상위  목록 표시 명칭 변경

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,10 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "DIRECTORY",
+      useSavedState: true,
+    }),
   ],
   right: [
     Component.Graph(),
